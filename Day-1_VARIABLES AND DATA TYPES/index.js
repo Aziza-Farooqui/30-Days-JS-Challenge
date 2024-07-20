0e811e604426e9d6912d01fae112d26914d7dfc2
Aziza-Farooqui-au50 <azizafarooqui2@gmail.com>
2024-07-20 19:41:17 +0530

    Day 1 Task completed

---
+++ Day-1_VARIABLES AND DATA TYPES/index.js
@@ -0,0 +1,46 @@
+//DAY 1: VARIABLES AND DATA TYPES
+
+//Activity 1: Variable Declaration
+//• Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+var a=10;
+console.log(a);
+
+//• Task 2: Declare a variable using let, assign it a string, and log the value to the console.
+let fruit="Apple";
+console.log(fruit);
+
+//Activity 2: Constant Declaration
+//• Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
+const b=false;
+console.log(b);
+
+//Activity 3: Data Types
+//• Task 4: Create variables of different data types (number, string, boolean, object, array) and log each variable's type using the typeof operator. Activity 4: Reassigning Variables
+var num=1;
+console.log(typeof one);
+
+var str="two";
+console.log(typeof two);
+
+const bool=true;
+console.log(typeof three);
+
+let person={
+    name:"Aziza",
+    age:24
+}
+console.log(typeof four);
+
+let arr=[7,17,77,"20"];
+console.log(typeof five); 
+
+//• Task 5: Declare a variable using let, assign it an initial value, reassign a new value, and log both values to the console.
+let num1=10;
+six=17;   
+console.log(six);
+//Activity 5: Understanding const
+
+//• Task 6: Try reassigning a variable declared with const and observe the error.
+const text=" I'm good today ";
+text=" I'm not good today ";
+console.log(text);
